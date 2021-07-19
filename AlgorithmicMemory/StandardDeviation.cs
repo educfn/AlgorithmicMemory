94e304ce23c2716324838119a8995c7bc50f2873
Eduardo CFN <eduardo@verion.ind.br>
2021-07-19 09:50:15 -0300

    Created the class 'StandardDeviation'.

---
+++ AlgorithmicMemory/StandardDeviation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmicMemory
+{
+    class StandardDeviation
+    {
+        //TODO: Calculate 'standart deviation'. Research how the calculus is made and create TODO´s
+        //which represent the stages of the calculation.
+    }
+}
