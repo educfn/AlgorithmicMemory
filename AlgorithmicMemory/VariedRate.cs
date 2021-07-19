a7595399676ac5a05342a0a80624e6850430ac54
Eduardo CFN <eduardo@verion.ind.br>
2021-07-19 11:07:35 -0300

    Add VariedRate class.

---
+++ AlgorithmicMemory/VariedRate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmicMemory
+{
+    class VariedRate
+    {
+        //TODO: Calculate 'VariedRate'. Research how the calculus is made and create TODO´s
+        //which represent the stages of the calculation.
+    }
+}
